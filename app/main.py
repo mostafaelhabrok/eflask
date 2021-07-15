68fbedc4476297d1675272791d3cb58b526bb63f
mostafaelhabrok <mostafa.elhabrok@gmail.com>
2021-07-15 18:54:26 +0200

    2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def home_view():
-		return "<h1>Welcome to Geeks for Geeks</h1>"
+ 
+@app.route('/')
+def index():
+  return '<h1>I want to Deploy Flask to Heroku</h1>'
